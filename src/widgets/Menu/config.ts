4ddbeb12d7905d779c7a28d964d22555325baac7
heheboix <84672799+heheboix@users.noreply.github.com>
2021-05-28 11:37:12 +0530

    Update config.ts

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -18,77 +18,7 @@ export const links = [
       },
     ],
   },
-  {
-    label: "Farms",
-    icon: "FarmIcon",
-    href: "/farms",
-  },
-  {
-    label: "Pools",
-    icon: "PoolIcon",
-    href: "/syrup",
-  },
-  {
-    label: "Lottery",
-    icon: "TicketIcon",
-    href: "/lottery",
-  },
-  {
-    label: "NFT",
-    icon: "NftIcon",
-    href: "/nft",
-  },
-  {
-    label: "Profile & Teams",
-    icon: "GroupsIcon",
-    items: [
-      {
-        label: "Leaderboard",
-        href: "/",
-      },
-      {
-        label: "YourProfile",
-        href: "/",
-      },
-    ],
-    calloutClass: "rainbow",
-  },
-  {
-    label: "Info",
-    icon: "InfoIcon",
-    items: [
-      {
-        label: "Overview",
-        href: "https://pantherswap.info",
-      },
-      {
-        label: "Tokens",
-        href: "https://pantherswap.info/tokens",
-      },
-      {
-        label: "Pairs",
-        href: "https://pantherswap.info/pairs",
-      },
-      {
-        label: "Accounts",
-        href: "https://pantherswap.info/accounts",
-      },
-    ],
-  },
-  {
-    label: "IFO",
-    icon: "IfoIcon",
-    items: [
-      {
-        label: "Next",
-        href: "/ifo",
-      },
-      {
-        label: "History",
-        href: "/ifo/history",
-      },
-    ],
-  },
+ 
   {
     label: "More",
     icon: "MoreIcon",
